7e0a5c5249884e4a63214354a0cffb04d75fc3b7
GrazielaPaola <grazielapaola48@gmail.com>
2025-02-27 20:55:25 -0300

    atualiza o ecommerce

--- produto/models.py
+++ produto/models.py
@@ -26,6 +26,20 @@ class Produto(models.Model):
     def resize_image(img, new_width=800):
         img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
         img_pil = Image.open(img_full_path)
+        original_width, original_height = img_pil.size
+
+        if original_width <= new_width:
+            img_pil.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+
+        new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+        new_img.save(
+            img_full_path,
+            optimeze=True,
+            quality=50
+        )
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
@@ -39,12 +53,16 @@ class Produto(models.Model):
         return self.nome
 
 
-"""
+class Variacao(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
 
-        Variacao:
-            nome - char
-            produto - FK Produto
-            preco - Float
-            preco_promocional - Float
-            estoque - Int
-"""
+    def __str__(self):
+        return self.nome or self.produto.nome
+
+    class Meta:
+        verbose_name = 'Variação'
+        verbose_name_plural = 'Variações'
